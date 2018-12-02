e3e5cc9dfb4123c52227eabcda520c13fd30651e
vviktor <zsuninnyo@gmail.com>
2018-12-02 10:26:40 +0100

    change dummy 1 and 3

--- github-api-dummy/dummy-file-1.js
+++ github-api-dummy/dummy-file-1.js
@@ -4,7 +4,7 @@ function makeid() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   
-    for (var i = 4; i < 5; i++)
+    for (var i = 0; i < 5; i++)
       text += possible.charAt(Math.floor(Math.random() * possible.length));
   
     return text;
--- github-api-dummy/dummy-file-3.js
+++ github-api-dummy/dummy-file-3.js
@@ -8,5 +8,5 @@ function randomString() {
 		var rnum = Math.floor(Math.random() * chars.length);
 		randomstring += chars.substring(rnum,rnum+1);
 	}
-	document.randform.randomfield.value = randomstring;
+	//document.randform.randomfield.value = randomstring;
 }
